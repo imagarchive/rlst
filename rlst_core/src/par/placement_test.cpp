dd5ef655b830bc3a2b1326e401975c8539840c7a
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2025-05-18 16:20:35 +0200

    `rlst::par::placement_grid`: fix unit tests
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- rlst_core/src/par/placement_test.cpp
+++ rlst_core/src/par/placement_test.cpp
@@ -184,7 +184,7 @@ TEST(placement_grid__overlap_penalty__test, full_overlap)
   };
 
   placement_grid g(10_ul, 10_ul, cells.begin(), cells.end());
-  ASSERT_DOUBLE_EQ(g.overlap_cost(), 2'000._r / static_cast<real_t>(g.size()));
+  ASSERT_DOUBLE_EQ(g.overlap_cost(), 4'000._r / static_cast<real_t>(g.size()));
 }
 
 TEST(placement_grid__overlap_penalty__test, partial_overlap)
@@ -195,7 +195,7 @@ TEST(placement_grid__overlap_penalty__test, partial_overlap)
   };
 
   placement_grid g(10_ul, 10_ul, cells.begin(), cells.end());
-  ASSERT_DOUBLE_EQ(g.overlap_cost(), 1'000._r / static_cast<real_t>(g.size()));
+  ASSERT_DOUBLE_EQ(g.overlap_cost(), 2'000._r / static_cast<real_t>(g.size()));
 }
 
 TEST(placement_grid__overlap_penalty__test, many)
@@ -208,7 +208,7 @@ TEST(placement_grid__overlap_penalty__test, many)
   };
 
   placement_grid g(10_ul, 10_ul, cells.begin(), cells.end());
-  ASSERT_DOUBLE_EQ(g.overlap_cost(), 3'000._r / static_cast<real_t>(g.size()));
+  ASSERT_DOUBLE_EQ(g.overlap_cost(), 6'000._r / static_cast<real_t>(g.size()));
 }
 
 TEST(row_length_penalty__basics__test, empty)
